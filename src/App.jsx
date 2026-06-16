43c1802d5341df37372121d50cef58e3983c55f8
whosprachi <prachisutar004@gmail.com>
2026-06-16 11:53:31 +0500

    add movie search functionality and search button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 
-import './App.css'
 import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
 function App() {
+     const movieNumber = 1;
+
+     if(movieNumber=== 1){}
   return(
    <>
-   <MovieCard movie={{title:"Tims film", release_date:"2024"}}/>
+   {movieNumber === 1? (
+     <MovieCard movie={{title:"Tims film", release_date:"2024"}}/>
+   ):(
    <MovieCard movie={{title:"Johns film", release_date:"2020"}}/>
+   )} 
 
-   
+   <Home/>
    </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,27 @@ function Home(){
     {id:1, title:"John wick", release_date:"2020"},
     {id:2, title:"Terminator", release_date:"1999"},
     {id:3, title:"Interstellar", release_date:"1998"},
-   ]
+   ];
+const handleSearch =() =>{};
+    return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form" >
+        <input 
+        type="text" 
+        placeholder="search for movies..." 
+        className="search-input"
+        />
+        <button type="submit" className="search-button">Search</button>
 
-    return <div className="home">
+      </form>
    <div className="movies-grid">
-    {movies.map((movie)=>(<
-        MovieCard movie={movie} key={movie.id}/>
+    {movies.map((movie)=>(
+        <MovieCard movie={movie} key={movie.id}/>
     ))}
      
    </div>
-    </div>
+</div>
+);
 }
 
 export default Home
