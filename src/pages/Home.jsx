1537dc9d7f8382f7abb99e6373496ee9519b40f3
whosprachi <prachisutar004@gmail.com>
2026-06-16 12:11:01 +0500

    Learnt basics of React State with onChange,preventDefault,setSearchQuery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import MovieCard from "../components/MovieCard"
+import {useState} from 'react'
 function Home(){
+     const [searchQuery,setSearchQuery] = useState("");
    const movies=[
     {id:1, title:"John wick", release_date:"2020"},
     {id:2, title:"Terminator", release_date:"1999"},
     {id:3, title:"Interstellar", release_date:"1998"},
    ];
-const handleSearch =() =>{};
+const handleSearch =(e) =>{
+    e.preventDefault()
+    alert(searchQuery)
+    setSearchQuery("");
+};
     return (
     <div className="home">
       <form onSubmit={handleSearch} className="search-form" >
@@ -13,6 +19,8 @@ const handleSearch =() =>{};
         type="text" 
         placeholder="search for movies..." 
         className="search-input"
+        value={searchQuery}
+        onChange={(e)=>setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">Search</button>
 
